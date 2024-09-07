9d9c93e348a59e509e75b4a7e19e667fa003eb46
alfred <lianghongbuaa@gmail.com>
2024-09-07 10:57:41 +0800

    add ci

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,23 @@ const nextConfig = {
   //   defaultLocale: 'zh'
   // },
   output: 'export',
-  reactStrictMode: false
+  reactStrictMode: false,
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  // basePath: "/nextjs-github-pages",
 };
 
 export default nextConfig;
