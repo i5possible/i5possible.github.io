88abb551fdba8bb6bd43e72ef5d06eda9f219a5e
alfred <lianghongbuaa@gmail.com>
2024-09-17 09:22:12 +0800

    add plop post config

---
+++ plopfile.mjs
@@ -0,0 +1,44 @@
+export default function (plop) {
+  // create your generators here
+  plop.setGenerator('post', {
+    description: 'generate empty posts',
+    prompts: [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'The title of the post:',
+        validate: function (input) {
+          if (input.trim() === '') {
+            return 'The title is required';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'date',
+        message: 'The date of the post (YYYY-MM-DD):',
+        default: new Date().toISOString().split('T')[0],
+      },
+      {
+        type: 'input',
+        name: 'expert',
+        message: 'The expert of the post:',
+      },
+    ],
+    actions: (data) => {
+      return [
+        {
+          type: 'add',
+          path: 'src/posts/en/{{kebabCase title}}.mdx',
+          templateFile: 'plop-templates/post.mdx.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/posts/zh/{{kebabCase title}}.mdx',
+          templateFile: 'plop-templates/post.mdx.hbs',
+        },
+      ]
+    },
+  })
+}
